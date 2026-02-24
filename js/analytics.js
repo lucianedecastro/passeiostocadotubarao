22d87fd06161b7d28c2774a6b7e567b0e82ffa98
Luciane <luciane.castro@gmail.com>
2026-02-24 12:05:30 -0300

    refactor: centralize GA4 setup and remove unused scripts

---
+++ js/analytics.js
@@ -0,0 +1,12 @@
+// Google Analytics 4 - Toca do Tubarão
+
+const GA_ID = "G-L8JJEB8HJL";
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag(){
+  dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', GA_ID);
